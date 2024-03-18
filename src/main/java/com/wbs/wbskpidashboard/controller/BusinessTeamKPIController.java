59c2bcc2e32e990811714398d02b540dd2fa869b
jcoder2020 <jcoder2022@gmail.com>
2024-03-18 14:38:52 +0000

    01-wbk-businesslogic: ADDED method getBusinessteam info

--- src/main/java/com/wbs/wbskpidashboard/controller/BusinessTeamKPIController.java
+++ src/main/java/com/wbs/wbskpidashboard/controller/BusinessTeamKPIController.java
@@ -1,9 +1,17 @@
 package com.wbs.wbskpidashboard.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wbs.wbskpidashboard.model.BusinessTeam;
+import com.wbs.wbskpidashboard.model.BusinessTeamKPI;
 import com.wbs.wbskpidashboard.repository.BusinessTeamKPIRepository;
+import com.wbs.wbskpidashboard.repository.BusinessTeamRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +22,32 @@ public class BusinessTeamKPIController {
 
     @Autowired
     private BusinessTeamKPIRepository businessTeamKPIRepository;
+
+    @Autowired
+    private BusinessTeamRepository businessTeamRepository;
+
     
     @GetMapping("wbs-kpi-dashboard/team/{teamId}")
-    public String getMethodName(@PathVariable Integer teamId) {
+    public List<BusinessTeamKPI> getMethodName(@PathVariable Long teamId) {
+        
+        List<BusinessTeamKPI> businessTeamKPIs = new ArrayList<>();
+        Optional<BusinessTeam> businessTeam = businessTeamRepository.findById(teamId);
 
-        return new String();
+        if(businessTeam.isPresent()){
+           return businessTeamKPIRepository.findByTeamName(businessTeam.get().getTeamName());
+        }
+        else return businessTeamKPIs  ;  
+        
     }
+    
+
+    @GetMapping("wbs-kpi-dashboard/kpi/ranking")
+
+    public Map<String, > getTeamRanking(){
+
+        
+        return null;
+    }
+
     
 }
--- src/main/java/com/wbs/wbskpidashboard/repository/BusinessTeamKPIRepository.java
+++ src/main/java/com/wbs/wbskpidashboard/repository/BusinessTeamKPIRepository.java
@@ -1,13 +1,24 @@
 package com.wbs.wbskpidashboard.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.wbs.wbskpidashboard.model.BusinessTeamKPI;
+import java.util.List;
+
 
 @Repository
 public interface BusinessTeamKPIRepository extends CrudRepository <BusinessTeamKPI,Long>{
     
-    
-    
+    List<BusinessTeamKPI> findByTeamName(String teamName);
+
+//     private String kpiBusinessMeasure; 		
+//    private double value;
+//    private String teamName;
+   
+    List<BusinessTeamKPI> findByKpiBusinessMeasure(String kpiBusinessMeasure);     
+
+    @Query(value = "SELECT team_name,kpi_business_measure , AVG(kpi.value) FROM business_team_kpi kpi" , nativeQuery = true)
+    List<BusinessTeamKPI> findAverageTeamKPIs();
 }

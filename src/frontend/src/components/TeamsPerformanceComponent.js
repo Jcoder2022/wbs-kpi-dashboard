e6a5b7eda3e0ce803a9449dd2b12bc58f2a38dd4
jcoder2020 <jcoder2022@gmail.com>
2024-03-20 16:48:21 +0000

    FE: ADDED Table that display overall average and individual average for all KPI measures against each team.

--- src/frontend/src/components/TeamsPerformanceComponent.js
+++ src/frontend/src/components/TeamsPerformanceComponent.js
@@ -1,13 +1,17 @@
-import {React,useEffect} from 'react'
+import {React,useEffect,useState} from 'react'
 
 const TeamsPerformanceComponent = () => {
+
+    const[summary,setSummary]=useState({});
+
     useEffect(
 
         ()=> {
             const fetchPerformanceSummary = async ()=>{
                 const response = await fetch ('http://localhost:8080/wbs-kpi-dashboard/kpi/summary');
-                const data = await response.json();
-                console.log(data);
+                const summary = await response.json();
+                //console.log(data);
+                setSummary(summary);
             }
             fetchPerformanceSummary();
         }
@@ -16,7 +20,48 @@ const TeamsPerformanceComponent = () => {
     
 
   return (
-    <div>TeamsPerformanceComponent</div>
+    
+    <div >
+            <table>
+                <thead>
+                 <tr>
+                    <th>Team Name</th>
+                    <th>Wacc</th>
+                    <th>Scores</th>
+                    <th>Factory Utilisation</th>
+                    <th>Employee Engagement</th>
+                    <th>Interest Coverage</th>
+                    <th>Marketing Spend/Rev</th>
+                    <th>eCar Sales</th>
+                    <th>CO2 Penalty</th>
+                    
+                </tr> 
+                </thead>
+                <tbody>
+                
+                {   ( Object.keys(summary).map(key=>
+                         <tr key={key}>
+                             <td>{key}</td>
+                             
+                             <td>{Object.entries( summary[key])[0][1]}</td>
+                             <td>{Object.entries( summary[key])[1][1]}</td>
+                             <td>{Object.entries( summary[key])[2][1]}</td>
+                             <td>{Object.entries( summary[key])[3][1]}</td>
+                             <td>{Object.entries( summary[key])[4][1]}</td>
+                             <td>{Object.entries( summary[key])[5][1]}</td>
+                             <td>{Object.entries( summary[key])[6][1]}</td>
+                             <td>{Object.entries( summary[key])[7][1]}</td>
+                             
+                         </tr>
+                     ))
+                }
+            
+            </tbody> 
+            </table>
+        </div>
+
+       
+
   )
 }
 
